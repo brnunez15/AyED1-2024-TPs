3a700ffc6431bce520434ea31136966fd277895c
brnunez15 <brnunez@uade.edu.ar>
2024-08-16 17:13:40 -0300

    first commit

---
+++ TP1/ej1.py
@@ -0,0 +1,10 @@
+marcas = []
+modelos = []
+puertas = []
+
+autos1 = [marcas, modelos, puertas]
+autos2 = [[], [], []]
+
+# Son matrices iguales o hay diferencias?
+print(autos1, id(autos1))
+print(autos2, id(autos2))
