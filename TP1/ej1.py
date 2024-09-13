774715b0731db19c8a43b7a94484a3596d70bfaa
brnunez15 <brnunez@uade.edu.ar>
2024-09-13 10:17:13 -0300

    ej1 modifique cosas

--- TP1/ej1.py
+++ TP1/ej1.py
@@ -1,12 +1,40 @@
-def mayor_de_tres (num1: int, num2: int, num3: int) -> int:
-    lista = [num1, num2, num3]
-    mayor = max(lista)
-    return mayor
+def maximo(a,b,c) -> int:
+    '''Devuelve el número máximo de los ingresado siempre que sea único.
+    Precondicion: deben ingresarse números enteros positivos.
+    Postcondicion: Devuelve el maximo estricto. De no ser asi, devuelve -1.
+    '''
+    if c < b:
+        if b < a:
+            max = a
+        else:
+            if b == a:
+                max = -1
+            else:
+                max = b
+    elif a < b:
+        if b < c:
+            max = c
+        else:
+            if b == c:
+                max = -1
+            else:
+                max = b
+    else:
+        if a == c:
+            max = -1
+    return max
 
-num1 = 4
-num2 = 90
-num3 = 9
 
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese un numero: "))
+c = int(input("Ingrese un numero: "))
 
-mayores = mayor_de_tres(num1, num2, num3)
-print(mayores)
+maximo_estricto = maximo(a,b,c)
+
+if maximo_estricto == -1:
+    print ("No existe un maximo unico")
+else:
+    if maximo_estricto > 0:
+        print (f"El numero maximo es:  {maximo_estricto}")
+    else:
+        print("El numero debe ser positivo")
